9670822096915292a1e7559ac8ef7622ce5d4f6c
gimsangjun <5321913@naver.com>
2023-05-27 16:10:22 +0900

    bug: 그 시간대의 예약이 있는지 체크하는 함수 오류 수정
    기존 조건 o.startTime BETWEEN :startTime AND :endTime
    -> ((:startTime BETWEEN o.startTime AND o.endTime) OR (:endTime BETWEEN o.startTime AND o.endTime)) 으로 변경

--- src/main/java/com/example/demo/repository/StudyRoomRepository.java
+++ src/main/java/com/example/demo/repository/StudyRoomRepository.java
@@ -8,13 +8,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public interface StudyRoomRepository extends JpaRepository<StudyRoom, Long>, JpaSpecificationExecutor<StudyRoom> {
     Optional<StudyRoom> findById(Long id);
-    @Query("SELECT o From Order o WHERE o.studyRoom = :studyRoom AND o.date = :date AND o.state != 'RETURN' AND o.state != 'CANCEL' AND o.startTime BETWEEN :startTime AND :endTime")
+    @Query("SELECT o From Order o WHERE o.studyRoom = :studyRoom AND o.date = :date AND o.state = 'NORMAL' AND ((:startTime BETWEEN o.startTime AND o.endTime) OR (:endTime BETWEEN o.startTime AND o.endTime))")
     List<Order> findByDateBetweenTime(@Param("studyRoom") StudyRoom studyRoom, @Param("date") LocalDate date, @Param("startTime") double startTime, @Param("endTime") double endTime);
 
     boolean existsByName(String name);
